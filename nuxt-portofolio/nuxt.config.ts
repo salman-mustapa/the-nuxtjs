53515f9b7496d183613df6395ceef3a18f6023ab
Salman Mustapa <salmanmustapa@outlook.com>
2025-09-21 11:34:44 +0800

    feat: portofolio -> starter project

---
+++ nuxt-portofolio/nuxt.config.ts
@@ -0,0 +1,19 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: {
+    enabled: true,
+
+    timeline: {
+      enabled: true
+    }
+  },
+
+  modules: [
+    '@nuxt/content',
+    '@nuxt/ui',
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/scripts'
+  ]
+})
